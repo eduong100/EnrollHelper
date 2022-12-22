badb8bb0e23b879f9022b444a4df767180c9b1a7
Ethan Duong <eduong@Ethans-MacBook-Pro.local>
2022-12-21 21:45:52 -0800

    Finish time converstion tests

--- src/content.js
+++ src/content.js
@@ -1,7 +1,9 @@
 import { getCalendarData, deployCalendar } from './utils/create_calendar';
+import { convertTime12to24Tests } from './utils/tests';
 const studyListTable = document.querySelector('#studylistTable');
 
 try {
+  convertTime12to24Tests();
   if (studyListTable) {
     let classes = getCalendarData(studyListTable);
     deployCalendar(studyListTable, classes);
